2ec10d45595b818319730debae5bfcea9d4716fe
matheusleo <matheusleotrab@gmail.com>
2022-09-27 20:31:22 -0300

    modify validator
    modify message from validator function

--- source/main.c
+++ source/main.c
@@ -6,7 +6,16 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:24:01 by mleonard          #+#    #+#             */
-/*   Updated: 2022/09/26 22:44:00 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/09/27 09:45:31 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fdf.h>
+
+int	main(int argc, char *argv[])
+{
+	int	map_fd;
+
+	map_fd = validate_user_input(argc, argv);
+	ft_printf("map_file %d\n", map_fd);
+}
--- source/validator.c
+++ source/validator.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 08:56:21 by mleonard          #+#    #+#             */
-/*   Updated: 2022/09/27 10:10:14 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/09/27 20:30:24 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	validate_user_input(int argc, char *argv[])
 	if (!extension || !(ft_strnstr(extension, ".fdf", 5) \
 		&& ft_strlen(extension) == 4))
 	{
-		ft_printf("Every map file should end with the extension'.fdf'\n");
+		ft_printf("File eror: map file should end with the extension'.fdf'\n");
 		exit(0);
 	}
 	map_fd = open(map_name, O_RDONLY);
