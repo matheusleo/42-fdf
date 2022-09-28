17c14df4ed53ac4e4ba4f0f755443e42e466dcec
matheusleo <matheusleotrab@gmail.com>
2022-09-27 23:06:16 -0300

    update fdf header
    update fdf header by adding window's macros, key code's macros, event code's macros + create basic struct to be used by the init_app function

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:38:21 by mleonard          #+#    #+#             */
-/*   Updated: 2022/09/27 10:00:00 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/09/27 22:33:32 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,36 @@
 # include "libft.h"
 # include <fcntl.h>
 # include <stdio.h>
+# include <mlx.h>
+
+typedef struct s_app
+{
+	void	*mlx;
+	void	*win;
+}				t_app;
+
+// General macros
+# define WIN_HEIGHT 540
+# define WIN_LENGTH 960
+
+// Key code macros
+# define ESC_CODE 65307
+# define W_CODE 119
+# define A_CODE 97
+# define S_CODE 115
+# define D_CODE 100
+# define PLUS_CODE 61
+# define MINUS_CODE 45
+# define UP_CODE 65362
+# define LEFT_CODE 65361
+# define DOWN_CODE 65364
+# define RIGHT_CODE 65363
+
+// Events code macros
+# define ON_DESTROY 17
+
+// Window management functions
+void	init_app();
 
 // User's input related functions
 int	validate_user_input(int argc, char *argv[]);
