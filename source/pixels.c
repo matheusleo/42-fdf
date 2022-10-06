6d892f93b84cbc60022890963ccf1845b712900f
matheusleo <matheusleotrab@gmail.com>
2022-10-05 23:59:37 -0300

    implement Bresenham's algorithm
    implement a first version of the Bresenham's line drawing algorithm

--- source/pixels.c
+++ source/pixels.c
@@ -6,11 +6,42 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 23:43:42 by mleonard          #+#    #+#             */
-/*   Updated: 2022/10/03 23:49:59 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/10/05 23:58:59 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fdf.h>/
+#include <fdf.h>
+
+/*
+	Bresenham's is an algorithim developed by the computer scientist
+	Jack Elton Bresenham to put lines into a raster by deciding
+	which pixel should be the next one based on the distance between the
+	current pixel (xi, yi) from the possible next - that can be (xi + 1, yi)
+	or (xi + 1, yi + 1)
+*/
+void	bresenham(t_app *app, int x_init, int y_init, int x_final, int y_final)
+{
+	int	d_x;
+	int	d_y;
+	int	p;
+
+	d_x = x_final - x_init;
+	d_y = y_final - y_init;
+	p = 2 * d_y - d_x;
+	while (x_init <= x_final)
+	{
+		put_pixel(app, x_init, y_init, RED_CODE);
+		x_init++;
+		if (p < 0)
+			p += 2 * (d_y);
+		else if (p >= 0)
+		{
+			p += 2 * (d_y - d_x);
+			y_init++;
+		}
+	}
+	mlx_put_image_to_window(app->mlx, app->win, app->img, 0, 0);
+}
 
 void	put_pixel(t_app *app, int x, int y, int color)
 {
