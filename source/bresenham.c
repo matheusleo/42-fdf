26302831b7a5977eedcdedc68120353605396c56
matheusleo <matheusleotrab@gmail.com>
2022-10-12 12:01:41 -0300

    update Bresenham's function
    update bresenham's algorithm to adequate it to the 42 Norme. However, it is still necessary to solve the problem with the 25 lines issue

--- source/bresenham.c
+++ source/bresenham.c
@@ -6,61 +6,74 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 20:57:06 by mleonard          #+#    #+#             */
-/*   Updated: 2022/10/10 13:06:59 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/10/12 11:59:54 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 
-/*
-	Bresenham's is an algorithim developed by the computer scientist
-	Jack Elton Bresenham to put lines into a raster by deciding
-	which pixel should be the next one based on the distance between the
-	current pixel (xi, yi) from the possible next - that can be (xi + 1, yi)
-	or (xi + 1, yi + 1)
-*/
-void	bresenham(t_app *app, int x_init, int y_init, int x_final, int y_final)
+static void	bresenham_fast_x(t_app *app, int x_i, int y_i, int x_f, int y_f)
 {
 	int		d_x;
 	int		d_y;
-	int		abs_d_x;
-	int		abs_d_y;
 	int		inc_x;
 	int		inc_y;
 	double	eps;
 
+	d_x = x_f - x_i;
+	d_y = y_f - y_i;
+	inc_x = ft_ternary(d_x > 0, 1, -1);
+	inc_y = ft_ternary(d_y > 0, 1, -1);
+	eps = 0;
+	while (ft_ternary(inc_x < 0, x_i >= x_f, x_i <= x_f))
+	{
+		put_pixel(app, x_i + 250, y_i + 250, RED_CODE);
+		eps += abs(d_y);
+		if (eps >= abs(d_x))
+		{
+			y_i += inc_x;
+			eps -= abs(d_x);
+		}
+		x_i += inc_x;
+	}
+}
+
+static void	bresenham_fast_y(t_app *app, int x_i, int y_i, int x_f, int y_f)
+{
+	int		d_x;
+	int		d_y;
+	int		inc_x;
+	int		inc_y;
+	double	eps;
+
+	d_x = x_f - x_i;
+	d_y = y_f - y_i;
+	inc_x = ft_ternary(d_x > 0, 1, -1);
+	inc_y = ft_ternary(d_y > 0, 1, -1);
+	eps = 0;
+	while (ft_ternary(inc_y < 0, y_i >= y_f, y_i <= y_f))
+	{
+		put_pixel(app, x_i + 250, y_i + 250, MANGO_CODE);
+		eps += abs(d_x);
+		if (eps >= abs(d_y))
+		{
+			x_i += inc_x;
+			eps -= abs(d_y);
+		}
+		y_i += inc_y;
+	}
+}
+
+void	bresenham(t_app *app, int x_init, int y_init, int x_final, int y_final)
+{
+	int	d_x;
+	int	d_y;
+
 	d_x = x_final - x_init;
 	d_y = y_final - y_init;
-	abs_d_x = abs(d_x);
-	abs_d_y = abs(d_y);
-	inc_x = d_x > 0 ? 1 : -1;
-	inc_y = d_y > 0 ? 1 : -1;
-	eps = 0;
-	if (abs_d_x > abs_d_y)
-	{
-		for (int x = x_init, y = y_init; inc_x < 0 ? x >= x_final : x <= x_final; x += inc_x)
-		{
-			put_pixel(app, x + 250, y + 250, RED_CODE);
-			eps += abs_d_y;
-			if (eps >= abs_d_x)
-			{
-				y += inc_y;
-				eps -= abs_d_x;
-			}
-		}
-	}
+	if (abs(d_x) > abs(d_y))
+		bresenham_fast_x(app, x_init, y_init, x_final, y_final);
 	else
-	{
-		for (int x = x_init, y = y_init; inc_y < 0 ? y >= y_final : y <= y_final; y += inc_y)
-		{
-			put_pixel(app, x + 250, y + 250, GREEN_CODE);
-			eps += abs_d_x;
-			if (eps >= abs_d_y)
-			{
-				x += inc_x;
-				eps -= abs_d_y;
-			}
-		}
-	}
+		bresenham_fast_y(app, x_init, y_init, x_final, y_final);
 	mlx_put_image_to_window(app->mlx, app->win, app->img, 0, 0);
 }
