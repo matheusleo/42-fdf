b58bf4e9f7a2582e459ee67e9be37ff34e740d4d
matheusleo <matheusleotrab@gmail.com>
2022-10-12 19:19:42 -0300

    modify app.c
    modify the app file to start using the function responsible for plotting the map

--- source/app.c
+++ source/app.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/27 22:04:26 by mleonard          #+#    #+#             */
-/*   Updated: 2022/10/01 14:59:04 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/10/12 12:18:07 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,7 @@ t_app	*init_app(t_fdf *fdf)
 	app->img_addr = mlx_get_data_addr(app->img, &(app->img_bpp), \
 		&(app->img_line_size), &(app->img_endian));
 	app->fdf = fdf;
+	put_map(app);
 	mlx_key_hook(app->win, key_handler, app);
 	mlx_hook(app->win, ON_DESTROY, 0L, shutdown_app, app);
 	mlx_loop(app->mlx);
