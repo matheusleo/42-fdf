63aac91d89a178ad1e61082368152433ac271b75
matheusleo <matheusleotrab@gmail.com>
2022-10-13 00:08:08 -0300

    fix put_pixels limits
    fix put_pixels limit to avoid letting them leak from one side of the screen to the other

--- source/pixels.c
+++ source/pixels.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 13:13:57 by mleonard          #+#    #+#             */
-/*   Updated: 2022/10/12 21:44:39 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/10/13 00:07:22 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	put_pixel(t_app *app, int x, int y, int color)
 	int		offset;
 	char	*real_pixel;
 
-	if (0 <= x && x <= WIN_WIDTH && 0 <= y && y <= WIN_HEIGHT)
+	if (0 < x && x < WIN_WIDTH && 0 < y && y < WIN_HEIGHT)
 	{
 		offset = y * app->img_line_size + x * (app->img_bpp / 8);
 		real_pixel = app->img_addr + offset;
