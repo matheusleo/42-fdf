096cc9610a967a15fe28e17da3a42b7d90bb43f0
matheusleo <matheusleotrab@gmail.com>
2022-10-12 22:32:24 -0300

    modify listiter
    modify lstiter to control the index of the current node from the linked list

--- source/utils.c
+++ source/utils.c
@@ -6,21 +6,24 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 11:50:00 by mleonard          #+#    #+#             */
-/*   Updated: 2022/10/12 12:36:53 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/10/12 22:31:44 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fdf.h>
 
-void	lstiter(t_list *lst, t_app *app, void (*f)(void *, void *))
+void	lstiter(t_list *lst, t_app *app, void (*f)(void *, void *, int))
 {
 	t_list	*cur_node;
+	int		node_pos;
 
 	cur_node = lst;
+	node_pos = 0;
 	while (cur_node && cur_node->content)
 	{
-		f(app, cur_node);
+		f(app, cur_node, node_pos);
 		cur_node = cur_node->next;
+		node_pos++;
 	}
 }
 
