2730b732f2255bce9ff1f01bb8065d7b70815e89
matheusleo <matheusleotrab@gmail.com>
2022-10-12 21:49:49 -0300

    modify put_pixels
    modify the function put_pixels to validate if the coordinates which were passed as argument are valid or not

--- source/pixels.c
+++ source/pixels.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 13:13:57 by mleonard          #+#    #+#             */
-/*   Updated: 2022/10/10 13:14:08 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/10/12 21:44:39 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	put_pixel(t_app *app, int x, int y, int color)
 	int		offset;
 	char	*real_pixel;
 
-	offset = y * app->img_line_size + x * (app->img_bpp / 8);
-	real_pixel = app->img_addr + offset;
-	*(unsigned int *)real_pixel = color;
+	if (0 <= x && x <= WIN_WIDTH && 0 <= y && y <= WIN_HEIGHT)
+	{
+		offset = y * app->img_line_size + x * (app->img_bpp / 8);
+		real_pixel = app->img_addr + offset;
+		*(unsigned int *)real_pixel = color;
+	}
 }
