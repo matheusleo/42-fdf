e14fab7aa2b6871115287ee60ee46b787dd32cb6
matheusleo <matheusleotrab@gmail.com>
2022-10-15 16:20:49 -0300

    update main
    update main after the addition of new props to the t_fdf struct

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 22:24:01 by mleonard          #+#    #+#             */
-/*   Updated: 2022/10/12 23:47:56 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/10/15 15:41:52 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,11 @@ static t_fdf	init_fdf(void)
 	fdf.edge_len = 15;
 	fdf.zoom = 1;
 	fdf.x_offset = 0;
-	fdf.y_offset = 0;
-	fdf.level_height = 1;
+	fdf.y_offset = -100;
+	fdf.level_height = fdf.edge_len;
+	fdf.angle_x = 0;
+	fdf.angle_y = 0;
+	fdf.angle_z = 0;
 	fdf.map_matrix = NULL;
 	return (fdf);
 }
@@ -31,11 +34,9 @@ static t_fdf	init_fdf(void)
 int	main(int argc, char *argv[])
 {
 	t_fdf	fdf;
-	t_app	*app;
-	char	*line;
 
 	fdf = init_fdf();
 	fdf.map_fd = validate_user_input(argc, argv);
 	fdf.map_matrix = parse_map(&fdf);
-	app = init_app(&fdf);
+	init_app(&fdf);
 }
